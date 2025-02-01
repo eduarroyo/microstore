ea389d8d0cc9bac9e13c74e79d21eaf199d43595
Eduardo Arroyo Ramírez <eduarroyo@outlook.com>
2025-02-01 09:12:15 +0100

    ShoppingCart and ShoppingCartItem models. Folder structure

---
+++ src/Services/Basket/Microstore.Service.BasketApi/Models/ShoppingCart.cs
@@ -0,0 +1,17 @@
+﻿namespace Microstore.Service.BasketApi.Models;
+
+public class ShoppingCart
+{
+    public string UserName { get; private set; } = default!;
+    public List<ShoppingCartItem> Items { get; set; } = new();
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+    
+    public ShoppingCart(string userName):this()
+    {
+        this.UserName = userName;
+    }
+
+    public ShoppingCart()
+    {
+    }
+}
