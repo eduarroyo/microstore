b727d9a59eede3f6dac4320291703e548c3e46cb
Eduardo Arroyo Ramírez <eduarroyo@outlook.com>
2025-02-01 12:11:55 +0100

    Fix ShoppingCartItem.ProductName type

--- src/Services/Basket/Microstore.Service.BasketApi/Models/ShoppingCartItem.cs
+++ src/Services/Basket/Microstore.Service.BasketApi/Models/ShoppingCartItem.cs
@@ -6,5 +6,5 @@ public class ShoppingCartItem
     public string Color { get; set; } = default!;
     public decimal Price { get; set; } = default!;
     public Guid ProductId { get; set; } = default!;
-    public int ProductName { get; set; } = default!;
+    public string ProductName { get; set; } = default!;
 }
