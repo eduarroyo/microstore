4150c114b67efe008510ffadba3673fe0e97e4a5
Eduardo Arroyo Ramírez <eduarroyo@outlook.com>
2025-02-01 11:09:58 +0100

    Delete basket endpoint

---
+++ src/Services/Basket/Microstore.Service.BasketApi/Basket/DeleteBasket/DeleteBasketCommandValidator.cs
@@ -0,0 +1,9 @@
+﻿namespace Microstore.Service.BasketApi.Basket.GetBasket;
+
+public class DeleteBasketCommandValidator: AbstractValidator<DeleteBasketCommand>
+{
+    public DeleteBasketCommandValidator()
+    {
+        RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required");
+    }
+}
---
+++ src/Services/Basket/Microstore.Service.BasketApi/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace Microstore.Service.BasketApi.Basket.GetBasket;
+
+public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+public record DeleteBasketResult(bool IsSuccess);
+
+public class DeleteBasketCommandHandler
+    : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+{
+    public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+    {
+        // TODO: delete basket from database
+        return new DeleteBasketResult(true);
+    }
+}
