33dba2c0377fef440848705f1b395ea8adac1431
Eduardo Arroyo Ramírez <eduarroyo@outlook.com>
2025-02-02 12:04:33 +0100

    Fix UserName setter

--- src/Services/Basket/Microstore.Service.BasketApi/Models/ShoppingCart.cs
+++ src/Services/Basket/Microstore.Service.BasketApi/Models/ShoppingCart.cs
@@ -4,7 +4,7 @@ namespace Microstore.Service.BasketApi.Models;
 
 public class ShoppingCart
 {
-    public string UserName { get; private set; } = default!;
+    public string UserName { get; set; } = default!;
     public List<ShoppingCartItem> Items { get; set; } = new();
     public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
     
