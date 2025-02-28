45cbefa545771565c3da67e59b2a8a72ffa21834
Eduardo Arroyo Ramírez <eduarroyo@gmail.com>
2025-02-28 10:12:26 +0100

    Delete order command

---
+++ src/Services/Ordering/Microstore.Service.OrderingApplication/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,3 @@
+﻿namespace Microstore.Service.OrderingApplication.Orders.Commands.DeleteOrder;
+public record DeleteOrderCommand(Guid OrderId) : ICommand<DeleteOrderResult>;
+public record class DeleteOrderResult(bool IsSuccess);
