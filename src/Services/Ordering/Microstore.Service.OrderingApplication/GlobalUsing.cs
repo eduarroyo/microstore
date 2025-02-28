f3dfed9c7531fca7f0e8157e8a84c12e2b90af22
Eduardo Arroyo Ramírez <eduarroyo@gmail.com>
2025-02-28 10:02:00 +0100

    UpdateOrder command

--- src/Services/Ordering/Microstore.Service.OrderingApplication/GlobalUsing.cs
+++ src/Services/Ordering/Microstore.Service.OrderingApplication/GlobalUsing.cs
@@ -1,7 +1,9 @@
 ﻿global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Exceptions;
 global using FluentValidation;
 global using Microsoft.EntityFrameworkCore;
-global using Microstore.Service.OrderingApplication.Dtos;
-global using Microstore.Service.OrderingDomain.Models;
 global using Microstore.Service.OrderingApplication.Data;
+global using Microstore.Service.OrderingApplication.Dtos;
+global using Microstore.Service.OrderingApplication.Exceptions;
+global using Microstore.Service.OrderingDomain.Models;
 global using Microstore.Service.OrderingDomain.ValueObjects;
---
+++ src/Services/Ordering/Microstore.Service.OrderingApplication/Orders/Commands/UpdateOrder/UpdateOrderCommand.cs
@@ -0,0 +1,4 @@
+﻿namespace Microstore.Service.OrderingApplication.Orders.Commands.UpdateOrder;
+
+public record UpdateOrderCommand (OrderDto Order) : ICommand<UpdateOrderResult>;
+public record UpdateOrderResult(bool IsSuccess);
