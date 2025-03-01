5f64131651ba6364c43044ecf2d2064d520af7c3
Eduardo Arroyo Ramírez <eduarroyo@gmail.com>
2025-03-01 10:02:41 +0100

    Domain events handlers

---
+++ src/Services/Ordering/Microstore.Service.OrderingApplication/Orders/EventHandlers/Domain/OrderUpdatedEventHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace Microstore.Service.OrderingApplication.Orders.EventHandlers.Domain;
+public class OrderCreatedEventHandler
+    (ILogger<OrderCreatedEventHandler> logger)
+    : INotificationHandler<OrderCreatedEvent>
+{
+    public Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Domain Event handled: {DomainEvent}", notification.GetType().Name);
+        return Task.CompletedTask;
+    }
+}
