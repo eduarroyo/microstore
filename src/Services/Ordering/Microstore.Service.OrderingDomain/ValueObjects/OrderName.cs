e52ef88f64c6e27d550cbda978aa62e7ca1be6d3
Eduardo Arroyo Ramírez <eduarroyo@gmail.com>
2025-03-23 18:13:24 +0100

    Remove name length validation

--- src/Services/Ordering/Microstore.Service.OrderingDomain/ValueObjects/OrderName.cs
+++ src/Services/Ordering/Microstore.Service.OrderingDomain/ValueObjects/OrderName.cs
@@ -2,7 +2,7 @@
 
 public record OrderName
 {
-    private const int DefaultLength = 5;
+    private const int DefaultLength = 3;
     public string Value { get; } = default!;
 
     private OrderName(string value) => Value = value;
@@ -10,7 +10,7 @@ public record OrderName
     public static OrderName Of(string value)
     {
         ArgumentException.ThrowIfNullOrEmpty(value, nameof(value));
-        ArgumentOutOfRangeException.ThrowIfNotEqual(value.Length, DefaultLength, nameof(value));
+        //ArgumentOutOfRangeException.ThrowIfNotEqual(value.Length, DefaultLength, nameof(value));
 
         return new OrderName(value);
     }
